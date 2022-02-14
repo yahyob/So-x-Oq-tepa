a7ec76665f27b3c2805a40965fc9bd9dbcc215ed
Mr-Dostonbek <goyipovdoston0@gmail.com>
2022-02-14 17:03:57 +0500

    .....

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import { useTranslation } from 'react-i18next';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import Banner from './pages/Banner';
 import NavbarMain from './pages/Navbar';
-import BoshqarmaHaqida from './pages/Boshqarma/BoshqarmaHaqida';
 import Home from './pages/Home';
+import BoshqarmaHaqida from './pages/Boshqarma/BoshqarmaHaqida';
+import Rahbariyat from './pages/Boshqarma/Rahbariyat';
+import BoshqarmaNizomi from './pages/Boshqarma/BoshqarmaNizomi';
+import BoshqarmaTuzilmasi from './pages/Boshqarma/BoshqarmaTuzilmasi';
+import SuvBolimlari from './pages/Boshqarma/SuvBolimlari';
+import MarkaziyApparat from './pages/Boshqarma/MarkaziyApparat';
+import ByudjetQonun from './pages/Boshqarma/ByudjetQonun';
+import OchiqMalumot from './pages/Boshqarma/OchiqMalumot';
+import ElektronJamiyat from './pages/Boshqarma/ElektronJamiyat';
+import BoshIshOrnlari from './pages/Boshqarma/BoshIshOrnlari';
+import TenderlarTanlovlar from './pages/Boshqarma/TenderlarTanlovlar';
+import Tadbirlar from './pages/Boshqarma/Tadbirlar';
+import Hayat from './pages/Boshqarma/Hayat';
 
+import Yangiliklar from './pages/Yangiliklar/Yangiliklar';
+import AxborotXizmati from './pages/Yangiliklar/AxborotXizmati';
+import Elonlar from './pages/Yangiliklar/Elonlar';
+import PresRelizlar from './pages/Yangiliklar/PresRelizlar';
+import OAVVakillar from './pages/Yangiliklar/OAV';
+
+import Fotogalereya from './pages/Mediagalereya/Fotogalereya';
+import VideoMaterial from './pages/Mediagalereya/VideoMaterial';
 
 function App() {
 
@@ -20,24 +40,27 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
-          <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
+          <Route path='raxbariyat' element={<Rahbariyat />} />
+          <Route path='boshqarmaNizomi' element={<BoshqarmaNizomi />} />
+          <Route path='boshqarmaTuzilmasi' element={<BoshqarmaTuzilmasi />} />
+          <Route path='hayat' element={<Hayat />} />
+          <Route path='suvBolimlari' element={<SuvBolimlari />} />
+          <Route path='markaziyAparat' element={<MarkaziyApparat />} />
+          <Route path='byudjetQonun' element={<ByudjetQonun />} />
+          <Route path='ochiqMalumotlar' element={<OchiqMalumot />} />
+          <Route path='elektronHukumat' element={<ElektronJamiyat />} />
+          <Route path='boshIsh' element={<BoshIshOrnlari />} />
+          <Route path='tenderlarTanlov' element={<TenderlarTanlovlar />} />
+          <Route path='tadbirlar' element={<Tadbirlar />} />
+
+          <Route path='Yangiliklar' element={<Yangiliklar />} />
+          <Route path='AxborotXizmati' element={<AxborotXizmati />} />
+          <Route path='Elonlar' element={<Elonlar />} />
+          <Route path='PresRelizlar' element={<PresRelizlar />} />
+          <Route path='OAVVakillar' element={<OAVVakillar />} />
+
+          <Route path='Fotogalereya' element={<Fotogalereya />} />
+          <Route path='VideoMaterial' element={<VideoMaterial />} />
           <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
           <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
           <Route path='boshqarmaHaqida' element={<BoshqarmaHaqida />} />
--- src/pages/Navbar/index.js
+++ src/pages/Navbar/index.js
@@ -7,48 +7,42 @@ const NavbarMain = () => {
         <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">
-                        <Link to="/">Logo</Link>
-                    </Navbar.Brand>
+                    {/* <Navbar.Brand href="#home"> */}
+                    <Link className="navbar-brand" to="/">Logo</Link>
+                    {/* </Navbar.Brand> */}
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
+
                             <NavDropdown title="Boshqarma" id="basic-nav-dropdown">
-                                <NavDropdown.Item>
-                                    <Link id="RouterNavLink" style={None} to="/boshqarmaHaqida">Boshqarma haqida</Link>
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    <Link to="/raxbariyat">Raxbariyat</Link>
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">
-                                    <Link to="/boshqarmaNizomi">Boshqarma nizomi</Link>
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">
-                                    <Link to="/boshqarmaTuzilmasi">Boshqarma tuzilmasi</Link>
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">
-                                    Hayat
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Suv bo’limlari</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Markaziy aparat</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Byujet to’grisidagi qonunchilik</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Ochiq ma’lumot</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Elektron hukumat</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Bo’sh ish o’rinlari</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Tenderlar va tanlovlar</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Tadbirlar</NavDropdown.Item>
+                                <Link className='dropdown-item' to="/boshqarmaHaqida">Boshqarma haqida</Link>
+                                <Link className='dropdown-item' to="/raxbariyat">Raxbariyat</Link>
+                                <Link className='dropdown-item' to="/boshqarmaNizomi">Boshqarma nizomi</Link>
+                                <Link className='dropdown-item' to="/boshqarmaTuzilmasi">Boshqarma tuzilmasi</Link>
+                                <Link className='dropdown-item' to="/hayat">Hayat</Link>
+                                <Link className='dropdown-item' to="/suvBolimlari">Suv bo’limlari</Link>
+                                <Link className='dropdown-item' to="/markaziyAparat">Markaziy aparat</Link>
+                                <Link className='dropdown-item' to="/byudjetQonun">Byujet to’grisidagi qonunchilik</Link>
+                                <Link className='dropdown-item' to="/ochiqMalumotlar">Ochiq ma’lumot</Link>
+                                <Link className='dropdown-item' to="/elektronHukumat">Elektron hukumat</Link>
+                                <Link className='dropdown-item' to="/boshIsh">Bo’sh ish o’rinlari</Link>
+                                <Link className='dropdown-item' to="/tenderlarTanlov">Tenderlar va tanlovlar</Link>
+                                <Link className='dropdown-item' to="/tadbirlar">Tadbirlar</Link>
                             </NavDropdown>
+
                             <NavDropdown title="Yangiliklar" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Yangiliklar</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Axborot xizmati</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">E'lonlar</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Pres relizlar</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">OAV vakillar</NavDropdown.Item>
+                                <Link className='dropdown-item' to="/Yangiliklar">Yangiliklar</Link>
+                                <Link className='dropdown-item' to="/AxborotXizmati">Axborot xizmati</Link>
+                                <Link className='dropdown-item' to="/Elonlar">E'lonlar</Link>
+                                <Link className='dropdown-item' to="/PresRelizlar">Pres relizlar</Link>
+                                <Link className='dropdown-item' to="/OAVVakillar">OAV vakillar</Link>
                             </NavDropdown>
+
                             <NavDropdown title="Mediagalereya" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Fotogalereya</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Video material</NavDropdown.Item>
+                                <Link className='dropdown-item' to="/Fotogalereya">Fotogalereya</Link>
+                                <Link className='dropdown-item' to="/VideoMaterial">Video material</Link>
                             </NavDropdown>
+
                             <NavDropdown title="Ma'lumotlar" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Suv xo’jaligi fidoiylari</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Korrupsiyaga qarshi kurash</NavDropdown.Item>
@@ -58,6 +52,7 @@ const NavbarMain = () => {
                                 <NavDropdown.Item href="#action/3.4">Rasmiy web sahifa</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.4">Axborotdan foydalanuvchilarning so’rovnomasi</NavDropdown.Item>
                             </NavDropdown>
+
                             <NavDropdown title="Hujjatlar" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Qonunlar</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Qarorlar</NavDropdown.Item>
@@ -66,12 +61,14 @@ const NavbarMain = () => {
                                 <NavDropdown.Item href="#action/3.4">Dasturlar</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.4">Loyihalar</NavDropdown.Item>
                             </NavDropdown>
+
                             <NavDropdown title="Interaktiv xizmatlar" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Jismoniy va yuridik shaxslar murojati</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Murojat yo’llash</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Tarjimai hol</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.4">Ish tartibi</NavDropdown.Item>
                             </NavDropdown>
+
                             <Nav.Link href="#link">Foydali maslahatlar</Nav.Link>
                             <Nav.Link href="#link">Bog’lanish </Nav.Link>
                         </Nav>
