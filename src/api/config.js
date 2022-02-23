efe764f47290326918c327bdfbbe2097192cd46d
Mr-Dostonbek <goyipovdoston0@gmail.com>
2022-02-23 09:29:57 +0500

    .....

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,13 @@
-impo
+import axios from "axios";
+import API_BASE_URL from './Urls'
+
+export const request = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Language': getLang()
+    }
+    // timeout: 10000,
+});
+
+console.log(request);
--- src/pages/Boshqarma/Rahbariyat/index.js
+++ src/pages/Boshqarma/Rahbariyat/index.js
@@ -10,7 +10,7 @@ import TabUnstyled from '@mui/base/TabUnstyled';
 
 const Rahbariyat = () => {
 
-    const { t } = useTranslation()
+    const { t } = useTranslation();
 
     return (
         <div>
